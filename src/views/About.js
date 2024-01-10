322a2c7df4629465770a4cf7999525908d337ad7
ericw142 <eric.h.warshawsky@gmail.com>
2024-01-10 15:37:16 -0500

    about content

--- src/views/About.js
+++ src/views/About.js
@@ -4,6 +4,10 @@ export const About = () => {
     return (
         <div className='content-body'>
             <h2 className='body-main-header'>about</h2>
+            <p><strong>We are the World Leader in Generic Products</strong></p>
+            <p>Established in 2024, based in the D.C. - Baltimore area</p>
+            <p>ShopSite ships some of your most desired products straight to your door, no matter where you live!</p>
+            <p>Today we have sold more than 3 million products online</p>
         </div>
     )
 }
