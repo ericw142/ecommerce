8b83d4803a38acf45db856c10b360025325d9fcd
ericw142 <eric.h.warshawsky@gmail.com>
2024-01-31 12:40:31 -0500

    change location button added to storefront

--- src/views/Storefront.js
+++ src/views/Storefront.js
@@ -22,20 +22,22 @@ export const Storefront = ({ locations, selectedStorefront, setSelectedStorefron
         <div className='container mb-5'>
             <ProductModal selectedProduct={selectedProduct} show={showModal} setShow={setShowModal} cart={cart} setCart={setCart}/>
             {!storefront ? (
-                <div>
-                    <div className="row">
-                        <div className="col">
-                            <select className="form-select" onChange={(e) => {setSelectedStorefront(e.target.value)}}>
-                                <option value="">Choose a location</option>
-                                {locations.map((el) => {
-                                    return <option value={el.location}>{el.location}</option>
-                                })}
-                            </select>
+                <div className='content-body' style={{marginTop: '100px'}}>
+                    <div className='container'>
+                        <div className="row">
+                            <div className="col">
+                                <select className="form-select" onChange={(e) => {setSelectedStorefront(e.target.value)}}>
+                                    <option value="">Choose a location</option>
+                                    {locations.map((el) => {
+                                        return <option value={el.location}>{el.location}</option>
+                                    })}
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row mt-2">
-                        <div className="col-6 mx-auto">
-                            <button className='btn btn-light w-100' onClick={() => {matchAndSetStorefront()}}>Go</button>
+                        <div className="row mt-2">
+                            <div className="col-6 mx-auto">
+                                <button className='btn btn-light w-100' onClick={() => {matchAndSetStorefront()}}>Go</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -43,7 +45,13 @@ export const Storefront = ({ locations, selectedStorefront, setSelectedStorefron
                 <div>
                     {/* display top row with search, storefront select, filter options */}
                     <div className='row'>
-                        <h6>{selectedStorefront}</h6>
+                        <div className='col text-start'>
+                            <button style={{marginTop: '16px'}} className='btn btn-outline-dark' onClick={() => setStorefront()}>&#x2190; change location</button>
+                        </div>
+                        <div className='col'>
+                            <h4 style={{marginTop: '16px'}}>{selectedStorefront}</h4>
+                        </div>
+                        <div className='col'></div>
                     </div>
                     {/* display product cards */}
                     <div className='row'>
