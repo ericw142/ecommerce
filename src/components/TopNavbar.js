3e000609a99f884341f1c6c5d0366eb7899edff5
ericw142 <eric.h.warshawsky@gmail.com>
2024-02-03 11:34:36 -0500

    style updates for navbar

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -5,32 +5,32 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export const TopNavbar = ({ selectedView, setSelectedView, cart }) => {
     return (
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-            <Container>
-            <Navbar.Brand>
-                        <button className='button-site-header mx-auto' onClick={() => setSelectedView('')}>
-                            E-Commerce Site
-                        </button>
-                    </Navbar.Brand>
+        <Navbar collapseOnSelect expand="lg" className="site-navbar">
+                <Navbar.Brand>
+                    <button className='button-site-header mx-auto white-text' onClick={() => setSelectedView('')}>
+                        E-Commerce Site
+                    </button>
+                </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#locations" onClick={() => setSelectedView('Locations')}>Locations</Nav.Link>
-                    <Nav.Link href="#about" onClick={() => setSelectedView('About')}>About</Nav.Link>
-                    <Nav.Link href="#careers" onClick={() => setSelectedView('Careers')}>Careers</Nav.Link>
-                    <Nav.Link href="#contact" onClick={() => setSelectedView('Contact')}>Contact</Nav.Link>
+                    <Nav.Link href="#locations" onClick={() => setSelectedView('Locations')}><span className='white-text'>Locations</span></Nav.Link>
+                    <Nav.Link href="#about" onClick={() => setSelectedView('About')}><span className='white-text'>About</span></Nav.Link>
+                    <Nav.Link href="#careers" onClick={() => setSelectedView('Careers')}><span className='white-text'>Careers</span></Nav.Link>
+                    <Nav.Link href="#contact" onClick={() => setSelectedView('Contact')}><span className='white-text'>Contact</span></Nav.Link>
                     </Nav>
                 <Nav>
                     {selectedView === 'Storefront' ? (
                         <Nav.Link href="#" onClick={() => setSelectedView('Cart')}>
-                            <p>Cart <HiMiniShoppingCart /><span className='cart-items-indicator'>{cart?.products?.length > 0 ? `( ${cart.products.length} )` : ''}</span></p>
+                            <p className='white-text m-0'>Cart <HiMiniShoppingCart /><span className='cart-items-indicator'>{cart?.products?.length > 0 ? `( ${cart.products.length} )` : ''}</span></p>
                         </Nav.Link>
                     ) : (
-                        <Nav.Link href="#" onClick={() => setSelectedView('Storefront')}>Store <FaShop /></Nav.Link>
+                        <Nav.Link href="#" onClick={() => setSelectedView('Storefront')}>
+                            <p className='white-text m-0'>Store <FaShop /></p>
+                        </Nav.Link>
                     )}
                 </Nav>
             </Navbar.Collapse>
-            </Container>
         </Navbar>
     )
 }
