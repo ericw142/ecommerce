2c7ddaa394364433b330a5887242848d8a262caf
ericw142 <eric.h.warshawsky@gmail.com>
2024-03-04 11:19:59 -0500

    image resizing

--- src/App.js
+++ src/App.js
@@ -9,13 +9,7 @@ function App() {
     const [cart, setCart] = useState({ products: [] });
 
     return (
-        <div className={
-            selectedView === 'Locations' ? "App animated-bg" : 
-            selectedView === 'About' ? 'App animated-bg' :
-            selectedView === 'Careers' ? 'App animated-bg' :
-            selectedView === 'Contact' ? 'App animated-bg' 
-            : "App"
-        }>
+        <div className="App">
             <TopNavbar selectedView={selectedView} setSelectedView={setSelectedView} cart={cart}/>
             <ContentBody
                 selectedView={selectedView}
@@ -24,22 +18,6 @@ function App() {
                 cart={cart}
                 setCart={setCart}
             />
-            {/* <div id="content-footer">
-                <div className='row'>
-                    <div className='col'>
-                        <button className="button-no-style mt-2" onClick={() => setSelectedView('Locations')}>Locations</button>
-                    </div>
-                    <div className='col'>
-                        <button className="button-no-style mt-2" onClick={() => setSelectedView('About')}>About</button>
-                    </div>
-                    <div className='col'>
-                        <button className="button-no-style mt-2" onClick={() => setSelectedView('Careers')}>Careers</button>
-                    </div>
-                    <div className='col'>
-                        <button className="button-no-style mt-2" onClick={() => setSelectedView('Contact')}>Contact</button>
-                    </div>
-                </div>
-            </div> */}
         </div>
     );
 }
