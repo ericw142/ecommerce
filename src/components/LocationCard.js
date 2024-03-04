fcba6afe7a87ea85e16d5a5a191cce8d1798aeda
ericw142 <eric.h.warshawsky@gmail.com>
2024-03-04 11:36:03 -0500

    removed tailwind, text sizing on locationcard

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const LocationCard = ({ location, storehours, address, setSelectedView, setSelectedStorefront }) => {
     return (
-        <div className='col-12 col-md-6'>
-            <div className='location-card card' onClick={() => {
+        <div className='col-12 col-md-6 text-start'>
+            <div className='location-card card p-3' onClick={() => {
                 setSelectedView('Storefront');
                 setSelectedStorefront(location);
             }}>
-                <p><strong>{location}</strong></p>
-                <p>{storehours}</p>
-                <p>{address}</p>
+                <p style={{fontSize: '28px'}}><strong>{location}</strong></p>
+                <p style={{fontSize: '18px'}}>{storehours}</p>
+                <p style={{fontSize: '18px'}}>{address}</p>
             </div>
         </div>
     )
