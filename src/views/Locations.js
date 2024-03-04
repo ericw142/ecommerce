0a631b4f448e571ad6915800e26ffb2c4871b9f7
ericw142 <eric.h.warshawsky@gmail.com>
2024-03-04 13:50:13 -0500

    Update Locations.js

--- src/views/Locations.js
+++ src/views/Locations.js
@@ -8,7 +8,7 @@ export const Locations = ({ locations, setSelectedView, setSelectedStorefront })
 
     return (
         <div className='content-body container' style={{marginTop: '100px'}}>
-            <h3 className='body-main-header'>locations</h3>
+            <h3 className='body-main-header'>locations </h3>
             <p>While our stores are located in the DC Baltimore area, we offer online shopping and prompt delivery, bringing our products to doorsteps across the country. Explore our selection in person or online!</p>
             <div className='row locations-row'>
                 {locations.map((el) => {
